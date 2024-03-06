2e9c1e723239a41b1b5a75cd61947105b6a7e88a
TURKI-HAMED <altassanturki@gmail.com>
2024-03-06 07:46:24 +0000

    8_commit__

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,14 +1,14 @@
 SELECT
-  o.orders_id
-  ,o.date_date
-  ,ROUND(CAST(o.margin AS FLOAT64) + s.shipping_fee - (s.logcost + s.ship_cost), 2) AS operational_margin 
-  ,o.quantity
-  ,o.revenue
-  ,o.purchase_cost
-  ,o.margin
-  ,s.shipping_fee
-  ,s.logcost
-  ,s.ship_cost
+  o.orders_id,
+  o.date_date,
+  ROUND(CAST(o.margin AS FLOAT64) + CAST(s.shipping_fee AS FLOAT64) - (CAST(s.logcost AS FLOAT64) + CAST(s.ship_cost AS FLOAT64)), 2) AS operational_margin,
+  CAST(o.quantity AS FLOAT64) AS quantity,
+  CAST(o.revenue AS FLOAT64) AS revenue,
+  CAST(o.purchase_cost AS FLOAT64) AS purchase_cost,
+  CAST(o.margin AS FLOAT64) AS margin,
+  CAST(s.shipping_fee AS FLOAT64) AS shipping_fee,
+  CAST(s.logcost AS FLOAT64) AS logcost,
+  CAST(s.ship_cost AS FLOAT64) AS ship_cost
 FROM {{ref("int_orders_margin")}} o
 LEFT JOIN {{ref("stg_raw__ship")}} s 
   USING(orders_id)
